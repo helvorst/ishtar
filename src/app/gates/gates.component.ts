6b227b924181491fc17539c009074928a54ab443
Elena Barantseva <ebarantseva@ebarantseva-macbookpro.roam.corp.google.com>
2018-09-26 15:45:25 +0200

    app

--- src/app/gates/gates.component.ts
+++ src/app/gates/gates.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-import { AnimalService } from 'src/app/animal.service';
-
+//import { AnimalService } from 'src/app/animal.service';
+declare var app: any;
 @Component({
   selector: 'app-gates',
   templateUrl: './gates.component.html',
-  styleUrls: ['./gates.component.css']
+  styleUrls: ['./gates.component.css'],
+  //providers: [{ provide: AnimalService, useClass: Logger }],
 })
 export class GatesComponent implements OnInit {
   animals:String[] = [];
+  usrname = 'unknown';
   
-  constructor(private as:AnimalService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.animals = this.as.getAnimalsForGates();
+    //private as:AnimalService
+    //this.animals = this.as.getAnimalsForGates();
+    this.usrname = app.user.email;
   }
 
 }
